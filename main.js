431d98e10fbc7b567dbb6bc92634ece3890aa017
Om Dhiraj <omdhiraj55@gmail.com>
2019-03-05 23:02:49 +0530

    final commit

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 
 let gridSize = 16;
 const container = document.querySelector("#container");
-let initial = 0;
+let index = 0;
+let select = 0;
 
 let squares = [];
+let squareR = [];
+let squareG = [];
+let squareB = [];
+
+const blackGrid = document.querySelector('#black');
+blackGrid.addEventListener('click', () => { resetBoxes(); select=0;});
+
+const colored = document.querySelector('#colored');
+colored.addEventListener('click', () => {resetBoxes(); select=1;});
+
+const resetBtn = document.querySelector('#reset');
 
 function getRandomInt(min,max){
     min = Math.ceil(min);
@@ -11,33 +23,62 @@ function getRandomInt(min,max){
     return Math.floor(Math.random()*(max -min +1)) + min;
 }
 
-
-for(let i = 0; i < gridSize; i++ ){
-
-    for(let j = 0; j < gridSize; j++ ){
-        
-        squares[(i * gridSize) + j] = document.createElement('div');
-        squares[(i * gridSize) + j].classList.add("pixel");
-        squares[(i * gridSize) + j].style.cssText = `height: ${700/gridSize}px; width: ${700/gridSize}px;
-            border-bottom: ${1/gridSize}px solid gray;  border-right: ${1/gridSize}px solid gray;`;
-        container.appendChild( squares[(i * gridSize) + j] );   
-    }
-    
+function resetBoxes(){
+    squares.forEach( (square) => {
+        square.style.background = "white";
+    });
 }
 
+function createSquares(){
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    for(let i = 0; i < gridSize; i++ ){
+
+        for(let j = 0; j < gridSize; j++ ){
+        
+            squares[(i * gridSize) + j] = document.createElement('div');
+            squares[(i * gridSize) + j].classList.add("pixel");
+            squares[(i * gridSize) + j].style.cssText = `height: ${700/gridSize}px; width: ${700/gridSize}px;
+                border-bottom: ${1/gridSize}px solid gray;  border-right: ${1/gridSize}px solid gray;`;
+            container.appendChild( squares[(i * gridSize) + j] );   
+        }
+    
+    }
 
 squares.forEach( (square) => {
-    square.addEventListener('mouseenter', (e) => {
-        e.target.style.background = 'black';
+        square.addEventListener('mouseenter', (e) => {
+           
+            if (select == 0)
+            e.target.style.background = 'black';
+            else {
+                index = squares.indexOf(e.target);
+              
+                if(e.target.style.background == "" || e.target.style.background == "white"){
+                    squareR[index]= getRandomInt(0,255);
+                    squareG[index]= getRandomInt(0,255);
+                    squareB[index]= getRandomInt(0,255);
+                    e.target.style.background = `rgb(${squareR[index]},${squareG[index]},${squareB[index]})`;
+                }
+                else{
+                squareR[index] = squareR[index] - (squareR[index]/10);
+                if(squareR[index]< 0) squareR[index] = 0;
+        
+                squareG[index] = squareG[index] - (squareG[index]/10);
+                if(squareG[index]< 0) squareG[index] = 0;
+        
+                squareB[index] = squareB[index] - (squareB[index]/10);
+                if(squareB[index]< 0) squareB[index] = 0;
+        
+                e.target.style.background = `rgb(${squareR[index]},${squareG[index]},${squareB[index]})`;
+                } 
+            }
+        });
     });
-});
-/*
-squares.forEach( (square) => {
-    square.addEventListener('mouseenter', (e) => {
-        if(e.target.style.background == "white")
-        e.target.style.background = `rgb(${getRandomInt(0,255)},${getRandomInt(0,255)},${getRandomInt(0,255)})`;
-        else
-        e.target.style.background =
-    });
-});
-*/
+
+}
+
+createSquares();
+
+resetBtn.addEventListener('click', () => {resetBoxes(); gridSize=prompt("Enter grid Size", 16);createSquares();});
+
